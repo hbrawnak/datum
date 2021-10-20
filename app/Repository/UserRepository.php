4c332e4ffb656b414da3e58b61d725cf8733d787
Habibur Rahman <hbrawnak@gmail.com>
2021-10-20 22:12:00 +0600

    Cache impletemented

--- app/Repository/UserRepository.php
+++ app/Repository/UserRepository.php
@@ -6,6 +6,7 @@ namespace App\Repository;
 
 use App\Contracts\UserRepositoryInterface;
 use App\Models\User;
+use Illuminate\Support\Facades\Cache;
 
 class UserRepository implements UserRepositoryInterface
 {
@@ -14,14 +15,22 @@ class UserRepository implements UserRepositoryInterface
      */
     private $user;
 
-    public function __construct(User $user)
+    /**
+     * @var Cache
+     */
+    private $cache;
+
+    public function __construct(User $user, Cache $cache)
     {
-        $this->user = $user;
+        $this->user  = $user;
+        $this->cache = $cache;
     }
 
 
     public function getUsers($offset = 0, $limit = 20)
     {
-        return $this->user->offset($offset)->limit($limit)->get();
+        return Cache::remember('users-' . $offset, 10, function () use ($offset, $limit) {
+            return $this->user->offset($offset)->limit($limit)->get();
+        });
     }
 }
