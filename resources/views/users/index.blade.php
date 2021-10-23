78aff1b66e58d2ae1d10b0359562749c1cc55f7b
Habibur Rahman <hbrawnak@gmail.com>
2021-10-23 11:43:39 +0600

    Pagination implemented in view

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -11,11 +11,13 @@
         <form class="row g-3" action="/" method="get">
             <div class="col-auto">
                 <label for="birthYear" class="visually-hidden">Birth Year</label>
-                <input type="text" name="year" class="form-control" id="birthYear" placeholder="Birth Year" value="{{ $year }}">
+                <input type="text" name="year" class="form-control" id="birthYear" placeholder="Birth Year"
+                       value="{{ $year }}">
             </div>
             <div class="col-auto">
                 <label for="birthMonth" class="visually-hidden">Birth Month</label>
-                <input type="text" name="month" class="form-control" id="birthMonth" placeholder="Birth Month" value="{{ $month  }}">
+                <input type="text" name="month" class="form-control" id="birthMonth" placeholder="Birth Month"
+                       value="{{ $month  }}">
             </div>
             <div class="col-auto">
                 <button type="submit" class="btn btn-warning mb-3">Filter</button>
@@ -27,6 +29,33 @@
     {{-- Datatable starts --}}
     @if (count($users) > 0)
         <div class="row">
+
+            <div class="d-flex justify-content-end">
+                <nav aria-label="Page navigation">
+                    <ul class="pagination">
+                        <li class="page-item {{ $users->previousPageUrl() ? '' : 'disabled' }}">
+                            <a class="page-link"
+                               href="{{ $users->appends(['year' => $year, 'month' => $month])->previousPageUrl() }}"
+                               aria-label="Previous">
+                                <span aria-hidden="true">&laquo;</span>
+                            </a>
+                        </li>
+
+                        <li class="page-item"><p class="page-link">{{ $users->firstItem()}} - {{$users->lastItem() }}
+                                of {{ $users->total() }}</p></li>
+
+                        <li class="page-item {{ $users->nextPageUrl() ? '' : 'disabled' }}">
+                            <a class="page-link"
+                               href="{{ $users->appends(['year' => $year, 'month' => $month])->nextPageUrl() }}"
+                               aria-label="Next">
+                                <span aria-hidden="true">&raquo;</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
+
             <table class="table">
                 <thead>
                 <tr>
@@ -54,8 +83,6 @@
                 @endforeach
                 </tbody>
             </table>
-            // TODO do pagination
-            {{--{{ $users->nextPageUrl() }}--}}
         </div>
     @else
         <p> No data to load. </p>
